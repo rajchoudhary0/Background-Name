67b8f526f6e5304237b7c8e617281301779e8bab
rajendra <rchoudhary157@gmail.com>
2020-10-01 21:34:21 +0530

    js modified

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const button = document.querySelector('button');
 const colors = ["red", "violet", "lightblue", "gold", "maroon", "aqua", "grey"];
 
 button.addEventListener('click', ()=> {
-    console.log("Clicked");
     const colorIndex = Math.floor(Math.random() * colors.length);
-    console.log(colorIndex);
     body.style.backgroundColor = colors[colorIndex];
 })
